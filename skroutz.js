b8b3cf7299d9bb22835c25fa5eef62c8c7108a8b
Nikos Katsos <nikoskeyz@gmail.com>
2018-08-24 19:29:51 +0300

    split logic into functions and parsed cards(dom elements) to objects

--- skroutz.js
+++ skroutz.js
@@ -1,18 +1,28 @@
 const SUPPORTED_MEAS_UNITS = ['ml', 'gr'];
 
 window.onload = () => {
-    const productCards = Array.from(document.querySelectorAll('.cf.card'));
-    for(const p of productCards) {
-        const titleElem = p.querySelector('.js-sku-link');
-        const title = titleElem.title;
-        console.log(title);
-
-        for(const m of SUPPORTED_MEAS_UNITS) {
-            const matches = new RegExp(`^.*\\s+(\\d*${m})(?:\\s|$)`, 'i').exec(title);
-            if (matches && matches.length) {
-                console.log(matches[1]);
-                break;
-            }
-        }
-    }
+    const productCards = getProductCards();
+    const products = productCards.map(getCardParsed);
+    console.log(products);
 };
+
+function getProductCards() {
+    return Array.from(document.querySelectorAll('.cf.card'));
+}
+
+function getCardParsed(cardElem) {
+    const titleElem = cardElem.querySelector('.js-sku-link');
+    const title = titleElem.title;
+    const { unit, size } = getSizeUnitOfMeasurement(title);
+
+    return { title, unit, size };
+}
+
+function getSizeUnitOfMeasurement(title) {
+    for(const unit of SUPPORTED_MEAS_UNITS) {
+        const matches = new RegExp(`^.*\\s+(\\d*${unit})(?:\\s|$)`, 'i').exec(title);
+        if (!matches || !matches.length) continue;
+        return { size: matches[1].split(unit)[0], unit };
+    }
+    return {};
+}
